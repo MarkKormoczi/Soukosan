0379fdf537e8db53a37e8c03f49a240b0b7f4d46
markkormoczi <mark@markkormoczi.com>
2018-03-18 16:34:13 +0100

    Added comments

--- src/com/coffeepp/Tester.java
+++ src/com/coffeepp/Tester.java
@@ -1,10 +1,16 @@
 package com.coffeepp;
 
+/**
+ * @author Mark
+ *Tester class, responsible for testing the listed cases.
+ */
+
 public class Tester {
     public Tester(){
 
     }
 
+    //Initialize a tester
     public void startTester(){
         System.out.println("Choose test case: ");
 
