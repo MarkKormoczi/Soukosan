d5753c09088ba5fe2cae569eca89631eb003db2b
markkormoczi <mark@markkormoczi.com>
2018-03-18 17:40:37 +0100

    Test cases declared

--- src/com/coffeepp/Tester.java
+++ src/com/coffeepp/Tester.java
@@ -1,20 +1,41 @@
 package com.coffeepp;
 
+import java.util.Scanner;
+
 /**
  * @author Mark
  *Tester class, responsible for testing the listed cases.
  */
 
 public class Tester {
+    Scanner sc = new Scanner(System.in);
     public Tester(){
+
 
     }
 
     //Initialize a tester
     public void startTester(){
         while(true){
-            System.out.println("Choose test case: " +
-                    "\t1 ");
+            System.out.print("Choose test case: \n" +
+                    "\t1 Initialize a map\n" +
+                    "\t2 Worker moves\n" +
+                    "\t3 Worker pushes Crate\n" +
+                    "\t4 Worker pushed\n" +
+                    "\t5 Crate pushed\n" +
+                    "\t6 Step to Floorbase\n" +
+                    "\t7 Update cycle\n" +
+                    "\t8 Switch\n" +
+                    "\t9 Add point\n" +
+                    "\t10 Destroy Crate\n" +
+                    "\t11 Destroy Worker\n" +
+                    "\t12 New game\n" +
+                    "\t13 Game over\n" +
+                    "\n\nEnter your choice: ");
+
+            String input = sc.nextLine();
+
+            System.out.println("You chose: " + input + "\n\n");
 
 
         }
