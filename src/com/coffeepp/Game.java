daa0ed39d1d11c9add57e5e6166b5a5e4f2df5bc
FeXyK <danyx000@gmail.com>
2018-03-18 23:52:55 +0100

    Beolvas 2.0

--- src/com/coffeepp/Game.java
+++ src/com/coffeepp/Game.java
@@ -9,7 +9,7 @@ public class Game {
     private WareHouse layout;
 
     private static Game instance = null;
-    private Game() {}
+    public Game() {}
 
     public static Game getInstance() {
         if(instance == null) {
@@ -31,7 +31,7 @@ public class Game {
         return currentLevel;
     }
     public void NewGame() throws IOException {
-        FileReader fr = new FileReader("map.txt");
+        FileReader fr = new FileReader("D:/map.txt");
         BufferedReader br = new BufferedReader(fr);
 
         StringBuilder sb = new StringBuilder();
@@ -45,23 +45,37 @@ public class Game {
 
         //mátrix feltöltése
         int row =0;
+        int mcol=0;
         while (line != null) {
-           row++;
+
            for(int col = 0; col< line.length(); col += 2)
            {
                char charfb = line.charAt(col);
                 switch(charfb) {
-                    case '0': FloorMatrix[row][col] = new Obstruction();
-                    case '1': FloorMatrix[row][col] = new Floor();
-                    case '2': FloorMatrix[row][col] = new TargetSpot();
-                    case '3': FloorMatrix[row][col] = new Switch();
-                    case '4': FloorMatrix[row][col] = new Trap();
-                    case '5': FloorMatrix[row][col] = new Hole();
+                    case '0': FloorMatrix[row][mcol] = new Obstruction();
+                    case '1': FloorMatrix[row][mcol] = new Floor();
+                    case '2': FloorMatrix[row][mcol] = new TargetSpot();
+                    case '3': FloorMatrix[row][mcol] = new Switch();
+                    case '4': FloorMatrix[row][mcol] = new Trap();
+                    case '5': FloorMatrix[row][mcol] = new Hole();
                 }
+               mcol++;
            }
+           row++;
+           mcol = 0;
             line = br.readLine();
         }
         br.close();
+        System.out.print("\n");
+
+        for(int i = 0; i < 10;i++)
+        {
+            for ( int j = 0; j<10; j++)
+            {
+                System.out.print(FloorMatrix[i][j]+" ");
+            }
+            System.out.print("\n");
+        }
 
     }
     public void GameOver()
--- src/com/coffeepp/Main.java
+++ src/com/coffeepp/Main.java
@@ -1,10 +1,20 @@
 package com.coffeepp;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.print("Sokoban-san");
         Logger.loggerInit();
+
+
+        Game g = new Game();
+        try {
+            g.NewGame();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         //Tester class call
         Tester tester = new Tester();
